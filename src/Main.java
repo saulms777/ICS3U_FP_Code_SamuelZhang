1b58e76bd47769f6f2b4eb734e287f9cc362af35
saulms <105379798+saulms777@users.noreply.github.com>
2023-01-22 17:06:45 -0500

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
 
+    /**
+     * Main method for program. Creates an instance of the class Game.
+     *
+     * @param args Unused
+     */
     public static void main(String[] args) {
         new Game();
     }
